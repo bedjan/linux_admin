6dd46d230686c6b71de2df64d0f797d5529dbb1d
bedjan <29821749+bedjan@users.noreply.github.com>
2025-03-03 09:05:58 +0100

    Update vytvoreni_a_zabezpeceni_ssh_klice.sh

--- skripty/vytvoreni_a_zabezpeceni_ssh_klice.sh
+++ skripty/vytvoreni_a_zabezpeceni_ssh_klice.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "Tento skript vytvoří nový SSH klíč a zabezpečí ho."
+
 read -p "Zadejte název souboru pro nový SSH klíč: " KEYNAME
 ssh-keygen -t rsa -b 4096 -f ~/.ssh/$KEYNAME -N ""
 echo "SSH klíč vytvořen: ~/.ssh/$KEYNAME"
